3ba4ce2d37eaec8ee75b16699ecefcefdfc50268
Jose Sanchez <jsanxez7@gmail.com>
2023-10-14 00:46:10 -0500

    wip: add generic files related with Button component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+import './styles/index.css'
+
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hola mundo</h1>
+    <div>
+      <Button />
+      <Button variant="filled-button"/>
+    </div>
   )
 }
 
