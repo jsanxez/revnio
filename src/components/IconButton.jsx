89c793c70a3c372e90edd545a893fffa12163137
Jose Sanchez <jsanxez7@gmail.com>
2023-10-16 23:55:17 -0500

    add disabled Icon Button and toggle option styles

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,12 +1,16 @@
-import "../styles/Icon-button.css"
+import '../styles/Icon-button.css'
 import '../styles/Outlined-icon.css'
 import '../styles/Tonal-icon.css'
 import '../styles/Filled-icon.css'
 
-export default function IconButton({iconName, variant, selected}) {
-  const state = selected ? '--selected' : '';
+export default function IconButton({ iconName, variant, selected=undefined, disabled }) {
+  let status = '';
+
+  if(selected === true)  status = '--selected';
+  if(selected === false) status = '--unselected';
+
   return(
-    <button className={`icon-button ${variant}-icon ${state}`}>
+    <button className={`icon-button ${variant}-icon ${status}`} disabled={disabled}>
       <span className="material-symbols-outlined">
         {iconName}
       </span>
