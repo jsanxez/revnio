69fdad2a615301bc49aaad6d7f9796b50e3e5eb3
Jose Sanchez <jsanxez7@gmail.com>
2023-11-09 21:03:52 -0500

    refactor (card): composable card
    It's necessary to define some card segments manually like content and actions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,19 @@
 import Button from './Button'
 import '../styles/Card.css'
 
-export default function Card({ headline, subhead }) {
+export function Card({ children }) {
   return(
     <div className='card'>
-      <div className='content'>
-        <div className='headline'>
-          <h4 className='title'>{headline}</h4>
-          <p className='subhead'>{subhead}</p>
-        </div>
-        <p className='text'>
-          cuestionario autorreportado que evalúa cómo las personas perciben el entorno y toman decisiones.
-        </p>
-        <div className='actions'>
-          <Button variant="filled" />
-        </div>
-      </div>
+      {children}
+    </div>
+  )
+}
+
+export function Headline({ title, subhead }) {
+  return(
+    <div className='headline'>
+      <h5 className='title'>{title}</h5>
+      {subhead && <h6 className='subhead'>{subhead}</h6>}
     </div>
   )
 }
